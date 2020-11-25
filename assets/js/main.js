2d01c4b8c9551efddba413e0bbaad4d84c80caf7
William Truppi <williamt@hotmail.it>
2020-11-25 19:28:39 +0100

    Refactoring
    il codice che effettua il controllo sui numeri "mina" è stato trasformato in una funziona, con conseguente adeguamento del codice

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,13 @@
 var userDifficultChoice;
 var difficult = 0;
 
-do{
-  userDifficultChoice = Number(prompt("Scegli tra i 3 livelli di difficoltà: [0] per EASY (numeri compresi tra 1 e 100), [1] per MEDIUM (numeri compresi tra 1 e 80, [2] per HARD (numeri compresi tra 1 e 50))"));
-  console.log(userDifficultChoice);
-} while (userDifficultChoice < 0 || userDifficultChoice > 2 || isNaN(userDifficultChoice))
+userDifficultChoice = Number(prompt("Scegli tra i 3 livelli di difficoltà: [0] per EASY (numeri compresi tra 1 e 100), [1] per MEDIUM (numeri compresi tra 1 e 80), [2] per HARD (numeri compresi tra 1 e 50)"));
+
+while (userDifficultChoice < 0 || userDifficultChoice > 2 || isNaN(userDifficultChoice)){
+  userDifficultChoice = Number(prompt("Hai inserito un valore errato, per favore attieniti alle indicazioni: [0] per EASY (numeri compresi tra 1 e 100), [1] per MEDIUM (numeri compresi tra 1 e 80), [2] per HARD (numeri compresi tra 1 e 50)"));
+}
+
+console.log(userDifficultChoice);
 
 switch (userDifficultChoice) {
   case 0:
@@ -60,20 +63,20 @@ do{
   }
   
   userNumbersArray.push(userNumber); 
-  
+                                
   // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 
-  for (var i = 0; i < cpuNumbersArray.length; i++){
-    if (cpuNumbersArray.includes(userNumber)){
-      alert("BOOOOOOOM BABY!! Hai beccato una mina dopo " + counter + " tentativi");
-      trovato = true;
-      break;
-    } else {
-      alert ("MINA EVITATA, vai avanti!!");
-      counter++;
-      break;
-    }
+  var checkArray = checkUserArray(cpuNumbersArray, userNumber); //invoco la funzione
+  console.log(checkArray);
+  
+  //effettuo il controllo sulla variabile booleana
+
+  if (trovato) {
+    alert("BOOOOOOOM BABY!! Hai beccato una mina dopo " + counter + " tentativi");
+  } else {
+    alert ("MINA EVITATA, vai avanti!!");
   }
+  
 } while (userNumbersArray.length < 5 && trovato == false)
 
 console.log(userNumbersArray);
@@ -87,3 +90,14 @@ if (userNumbersArray.length == 5 && trovato == false){
 function randomNumber(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min); //incluso sia il minimo che il massimo
 }
+
+function checkUserArray (array, number) {
+  for (var i = 0; i < array.length; i++){
+    if (array.includes(number)){
+      return trovato = true;
+    } else {
+      return counter++;
+    }
+  }
+}
+
